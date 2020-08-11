462b84362bb75a18828112689792dd59f2ab27f7
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2020-08-11 14:08:05 -0400

    complete

---
+++ src/main/java/org/launchcode/enumerableplanets/models/Planets.java
@@ -0,0 +1,28 @@
+package org.launchcode.enumerableplanets.models;
+
+public enum Planets {
+    MERCURY("Mercury", "88"),
+    VENUS("Venus", "225"),
+    EARTH("Earth", "365"),
+    MARS("Mars", "687"),
+    JUPITER("Jupiter", "4333"),
+    SATURN("Saturn", "10759"),
+    URANUS("Uranus", "30687"),
+    NEPTUNE("Neptune","60200");
+
+    private final String displayName;
+    private final String yearLength;
+
+    Planets(String displayName, String yearLength) {
+        this.displayName = displayName;
+        this.yearLength = yearLength;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public String getYearLength() {
+        return yearLength;
+    }
+}
